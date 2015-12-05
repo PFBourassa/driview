66ff6cf3170f6f3963bbe19be0308cb08cfcfd32
Parker work <parker@thepeaktv.com>
2015-12-05 11:22:29 -0500

    Can save and reopen state from txt file.

--- driview.py
+++ driview.py
@@ -2,8 +2,22 @@ import dfile
 import status
 
 
+
+
+def save_entire_state():
+    f = open('state', 'w')
+    s = str(state)
+    f.write(s)
+
+def open_state():
+    f = open('state', 'r').read()
+    state = eval(f)
+    return state
+    
 drive_list = dfile.get_drive_list()
 #dfile.print_all(drives)
 
-state = status.make_staus(drive_list)
-status.print_pretty(state)
+state1 = status.make_staus(drive_list)
+state2 = open_state()
+print (state1 == state2)
+#status.print_pretty(state)
